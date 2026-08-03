74e70a2bf946f7f0b5b016d65ccfacbfae4b1072
Dinesh Bhagwat <dinesh.bhagwat@gmail.com>
2026-08-03 08:17:43 +0530

    Restructered the code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,58 +3,71 @@ package org.example;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private int[] data;
-    private int numberOfElements;
     private int target;
+    private final Scanner scanner = new Scanner(System.in);
 
-    public int getTarget() {
-        return target;
-    }
-
-    public static void main() {
+    public static void main(String [] args) {
         Main main = new Main();
-        main.read();
-        main.print();
-        System.out.println("The indices which add up to " + main.getTarget() + " are " + Arrays.toString(main.twoSum()));
+
+        if (main.read()) {
+            main.print();
+            System.out.println("The two indices which add up to " + main.target + " are : " + Arrays.toString(main.twoSum()));
+        }
+        main.scanner.close();
     }
 
-    public void read() {
-        System.out.print("Enter the number of elements to be stored in an array : ");
-        Scanner scanner = new Scanner(System.in);
-        numberOfElements = scanner.nextInt();
-        data = new int[numberOfElements];
-        System.out.print("Read elements of the array one by one (each separated by a whitespace - blank/tab/new-line)): ");
+    public boolean read() {
+        System.out.print("Enter the number of elements you want to store in an array : ");
 
-        for (int i = 0; i < numberOfElements; i++) {
+        if (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid integer.");
+            return false;
+        }
+        final int numberOfElements = scanner.nextInt();
+
+        if (numberOfElements <= 0) {
+            System.out.println("Number of elements should be at least 1.");
+            return false;
+        }
+        data = new int[numberOfElements];
+        System.out.print("Enter the elements (each separated by a blank/tab/new-line): ");
+
+        for (int i = 0; i < data.length; i++) {
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please enter integers only.");
+                return false;
+            }
             data[i] = scanner.nextInt();
         }
         System.out.print("Enter the target : ");
+
+        if (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid integer.");
+            return false;
+        }
         target = scanner.nextInt();
-        scanner.close();
+        return true;
     }
 
     public void print() {
-        System.out.print("The array is as follows: ");
-        System.out.println(Arrays.toString(data));
+        System.out.println("The array is : " + Arrays.toString(data));
     }
 
+    // Check every pair of elements
     public int[] twoSum() {
-        int[] result = new int[2];
 
-        for (int i = 0; i < numberOfElements; i++) {
+        for (int i = 0; i < data.length; i++) {
 
-            for (int j = i + 1; j < numberOfElements; j++) {
+            for (int j = i + 1; j < data.length; j++) {
+
                 if (data[i] + data[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+                    return new int[] {i, j};
                 }
             }
-
         }
-        return result;
+        return new int[] {-1, -1};
     }
 }
